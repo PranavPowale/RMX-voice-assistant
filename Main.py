e2209cd381696b623489b82a1cde762f79d11d02
jackzbazuka <milindmoto99@gmail.com>
2021-04-03 12:31:25 +0530

    window config

--- Main.py
+++ Main.py
@@ -8,6 +8,7 @@ class Main(Tk):
         super().__init__(screenName=screenName, baseName=baseName, useTk=useTk, sync=sync, use=use)
 
         self.title("RMX")
+        self.geometry("600x600")
 
         self.f1 = Frame(master=self)
         
